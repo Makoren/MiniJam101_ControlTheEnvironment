21ae0de8f772082dfb7f61b7a19e0d8e8eb55412
Luke Lazzaro <zodiac44@hotmail.com>
2022-03-05 14:02:10 +1100

    Add ability to spread fires

--- main/objects/flammable_template.go
+++ main/objects/flammable_template.go
@@ -31,3 +31,31 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"object\"\n"
+  "mask: \"cursor\"\n"
+  "mask: \"fire\"\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- main/objects/wooden_box/wooden_box.go
+++ main/objects/wooden_box/wooden_box.go
@@ -61,6 +61,7 @@ embedded_components {
   "restitution: 0.5\n"
   "group: \"object\"\n"
   "mask: \"cursor\"\n"
+  "mask: \"fire\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
