fad8e7ef1485aa85175cc18da5fb71af82885473
Luke Lazzaro <zodiac44@hotmail.com>
2022-03-05 15:06:50 +1100

    Add plank, start work on draggable script

--- main/objects/plank/plank.go
+++ main/objects/plank/plank.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "draggable"
+  component: "/main/objects/draggable.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "collectionfactory"
   type: "collectionfactory"
@@ -42,6 +57,27 @@ embedded_components {
   "group: \"object\"\n"
   "mask: \"cursor\"\n"
   "mask: \"fire\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: -0.26633596\n"
+  "      w: 0.96388024\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 35.229\n"
+  "  data: 13.1255\n"
+  "  data: 10.0\n"
+  "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
   "locked_rotation: false\n"
@@ -71,6 +107,26 @@ embedded_components {
     x: 0.0
     y: 15.0
     z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/a_general.atlas\"\n"
+  "default_animation: \"plank\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
   }
   rotation {
     x: 0.0
