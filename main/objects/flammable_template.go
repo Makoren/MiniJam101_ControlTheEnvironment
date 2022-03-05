eda9bb019e4c0c966257b7331ed5430f820d9cdd
Luke Lazzaro <zodiac44@hotmail.com>
2022-03-05 11:28:05 +1100

    Add box sprite and fire animation

---
+++ main/objects/flammable_template.go
@@ -0,0 +1,33 @@
+components {
+  id: "flammable"
+  component: "/main/objects/flammable.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collectionfactory"
+  type: "collectionfactory"
+  data: "prototype: \"/main/objects/fire/fire.collection\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ main/objects/wooden_box/wooden_box.go
@@ -0,0 +1,53 @@
+components {
+  id: "flammable"
+  component: "/main/objects/flammable.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/a_general.atlas\"\n"
+  "default_animation: \"wooden_box\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collectionfactory"
+  type: "collectionfactory"
+  data: "prototype: \"/main/objects/fire/fire.collection\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
