15b2a18a1405a76bac17ee915e24ca2bd8468976
Luke Lazzaro <zodiac44@hotmail.com>
2022-03-07 14:07:51 +1100

    Add alert bubble

---
+++ main/objects/alert/alert.go
@@ -0,0 +1,35 @@
+components {
+  id: "alert"
+  component: "/main/objects/alert/alert.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/a_general.atlas\"\n"
+  "default_animation: \"alert_bubble\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 38.0
+    y: 94.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- main/objects/worker/worker.go
+++ main/objects/worker/worker.go
@@ -95,3 +95,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/main/objects/alert/alert.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
